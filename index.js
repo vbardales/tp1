e39d6fc7300f6d856decfe41f03280eeb1682393
vbardales <virginie.bardales@gmail.com>
2017-11-21 15:19:52 +0100

    etape 29

--- index.js
+++ index.js
@@ -1,20 +1,39 @@
 'use strict';
 
+const express = require('express');
+const mysql = require('mysql');
+const Promise = require('bluebird')
+
 console.log('Je suis content de faire du JS');
 
-var mysql      = require('mysql');
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : '',
-  database : 'cnam_2016',
+const connection = Promise.promisifyAll(mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'cnam_2016',
+}));
+
+const app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
 });
 
-connection.connect();
-
-connection.query('SELECT * from dessert', function (error, results, fields) {
-  if (error) throw error;
-  console.log('The solution is: ', results);
+app.get('/view', function (req, res) {
+  res.send('Hello World!');
 });
 
-connection.end();
+connection.connectAsync()
+  .then(function() {
+    app.listen(3000, function() {
+      console.log('Example app listening on port 3000!');
+      connection.queryAsync('SELECT * from dessert')
+        .then(function(results) {
+          console.log('The solution is: ', results);
+
+          connection.end();
+        })
+      ;
+    });
+  })
+;
